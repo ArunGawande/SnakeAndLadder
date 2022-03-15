579c7e36381530a3974af4d5f97689c9b3c020da
Arun <arungawande1995@gmail.com>
2022-03-15 13:33:24 +0530

    Added: SnakeAndLadderUC-2

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,11 +2,17 @@ package com.bridgelabz;
 
 public class SnakeAndLadder
 {
-        public static void main(String[] args) {
+    public static int getRandomInteger(int maximum, int minimum){
+        return ((int) (Math.random()*(maximum - minimum))) + minimum;
+    }
 
-            System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
+    public static void main(String[] args) {
 
-            int startPositionA = 0;
+        System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
 
-        }
+        int startPositionA = 0;
+        int dice = getRandomInteger(7,1);
+        System.out.println(dice);
+
+    }
 }
